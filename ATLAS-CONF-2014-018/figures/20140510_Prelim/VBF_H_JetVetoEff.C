c7a603c12b009811f5d1fe99aedd9c48ae513b8f
Pascal Nef <pascal.nef@gmail.com>
2014-12-15 15:25:41 -0800

    adding ATLAS-CONF-2014-018

---
+++ ATLAS-CONF-2014-018/figures/20140510_Prelim/VBF_H_JetVetoEff.C
@@ -0,0 +1,304 @@
+{
+//=========Macro generated from canvas: c1/c1
+//=========  (Sun May 11 01:39:20 2014) by ROOT version5.32/00
+   TCanvas *c1 = new TCanvas("c1", "c1",10,32,600,600);
+   gStyle->SetOptStat(0);
+   gStyle->SetOptTitle(0);
+   c1->SetHighLightColor(2);
+   c1->Range(-7.297297,-0.3355263,33.24324,1.638158);
+   c1->SetFillColor(0);
+   c1->SetBorderMode(0);
+   c1->SetBorderSize(2);
+   c1->SetTickx(1);
+   c1->SetTicky(1);
+   c1->SetLeftMargin(0.18);
+   c1->SetRightMargin(0.08);
+   c1->SetTopMargin(0.07);
+   c1->SetBottomMargin(0.17);
+   c1->SetFrameBorderMode(0);
+   c1->SetFrameBorderSize(0);
+   c1->SetFrameBorderMode(0);
+   c1->SetFrameBorderSize(0);
+   
+   TGraphErrors *gre = new TGraphErrors(6);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
+
+   Int_t ci;   // for color index setting
+   ci = TColor::GetColor("#660066");
+   gre->SetLineColor(ci);
+
+   ci = TColor::GetColor("#660066");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(20);
+   gre->SetPoint(0,2.5,0.8108289);
+   gre->SetPointError(0,0,0.02328085);
+   gre->SetPoint(1,7.5,0.7438325);
+   gre->SetPointError(1,0,0.01032901);
+   gre->SetPoint(2,12.5,0.6567098);
+   gre->SetPointError(2,0,0.006426298);
+   gre->SetPoint(3,17.5,0.5466152);
+   gre->SetPointError(3,0,0.006192375);
+   gre->SetPoint(4,22.5,0.4512079);
+   gre->SetPointError(4,0,0.009264444);
+   gre->SetPoint(5,27.5,0.3660221);
+   gre->SetPointError(5,0,0.02248456);
+   
+   TH1F *Graph_Graph1 = new TH1F("Graph_Graph1","Graph",100,0,30);
+   Graph_Graph1->SetMinimum(0);
+   Graph_Graph1->SetMaximum(1.5);
+   Graph_Graph1->SetDirectory(0);
+   Graph_Graph1->SetStats(0);
+   Graph_Graph1->GetXaxis()->SetTitle("N_{Vtx}");
+   Graph_Graph1->GetXaxis()->SetNdivisions(505);
+   Graph_Graph1->GetXaxis()->SetLabelFont(42);
+   Graph_Graph1->GetXaxis()->SetLabelSize(0.05);
+   Graph_Graph1->GetXaxis()->SetTitleSize(0.05);
+   Graph_Graph1->GetXaxis()->SetTitleOffset(1.1);
+   Graph_Graph1->GetXaxis()->SetTitleFont(42);
+   Graph_Graph1->GetYaxis()->SetTitle("jet veto efficiency");
+   Graph_Graph1->GetYaxis()->SetNdivisions(505);
+   Graph_Graph1->GetYaxis()->SetLabelFont(42);
+   Graph_Graph1->GetYaxis()->SetLabelSize(0.05);
+   Graph_Graph1->GetYaxis()->SetTitleSize(0.05);
+   Graph_Graph1->GetYaxis()->SetTitleOffset(1.5);
+   Graph_Graph1->GetYaxis()->SetTitleFont(42);
+   Graph_Graph1->GetZaxis()->SetNdivisions(707);
+   Graph_Graph1->GetZaxis()->SetLabelFont(42);
+   Graph_Graph1->GetZaxis()->SetLabelOffset(0.015);
+   Graph_Graph1->GetZaxis()->SetLabelSize(0.05);
+   Graph_Graph1->GetZaxis()->SetTitleSize(0.05);
+   Graph_Graph1->GetZaxis()->SetTitleOffset(1.1);
+   Graph_Graph1->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph1);
+   
+   gre->Draw("alp");
+   
+   gre = new TGraphErrors(6);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
+
+   ci = TColor::GetColor("#3300cc");
+   gre->SetLineColor(ci);
+
+   ci = TColor::GetColor("#3300cc");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(21);
+   gre->SetPoint(0,2.5,0.9304813);
+   gre->SetPointError(0,0,0.02493952);
+   gre->SetPoint(1,7.5,0.9323006);
+   gre->SetPointError(1,0,0.01156377);
+   gre->SetPoint(2,12.5,0.9230914);
+   gre->SetPointError(2,0,0.007618974);
+   gre->SetPoint(3,17.5,0.909786);
+   gre->SetPointError(3,0,0.007988882);
+   gre->SetPoint(4,22.5,0.8877687);
+   gre->SetPointError(4,0,0.01299514);
+   gre->SetPoint(5,27.5,0.8618785);
+   gre->SetPointError(5,0,0.03450275);
+   
+   TH1F *Graph_Graph2 = new TH1F("Graph_Graph2","Graph",100,0,30);
+   Graph_Graph2->SetMinimum(0.8145712);
+   Graph_Graph2->SetMaximum(0.9682253);
+   Graph_Graph2->SetDirectory(0);
+   Graph_Graph2->SetStats(0);
+   Graph_Graph2->GetXaxis()->SetNdivisions(505);
+   Graph_Graph2->GetXaxis()->SetLabelFont(42);
+   Graph_Graph2->GetXaxis()->SetLabelSize(0.05);
+   Graph_Graph2->GetXaxis()->SetTitleSize(0.05);
+   Graph_Graph2->GetXaxis()->SetTitleOffset(1.1);
+   Graph_Graph2->GetXaxis()->SetTitleFont(42);
+   Graph_Graph2->GetYaxis()->SetNdivisions(505);
+   Graph_Graph2->GetYaxis()->SetLabelFont(42);
+   Graph_Graph2->GetYaxis()->SetLabelSize(0.05);
+   Graph_Graph2->GetYaxis()->SetTitleSize(0.05);
+   Graph_Graph2->GetYaxis()->SetTitleOffset(1.5);
+   Graph_Graph2->GetYaxis()->SetTitleFont(42);
+   Graph_Graph2->GetZaxis()->SetNdivisions(707);
+   Graph_Graph2->GetZaxis()->SetLabelFont(42);
+   Graph_Graph2->GetZaxis()->SetLabelOffset(0.015);
+   Graph_Graph2->GetZaxis()->SetLabelSize(0.05);
+   Graph_Graph2->GetZaxis()->SetTitleSize(0.05);
+   Graph_Graph2->GetZaxis()->SetTitleOffset(1.1);
+   Graph_Graph2->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph2);
+   
+   gre->Draw("lp");
+   
+   gre = new TGraphErrors(6);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
+
+   ci = TColor::GetColor("#660066");
+   gre->SetLineColor(ci);
+
+   ci = TColor::GetColor("#660066");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(24);
+   gre->SetPoint(0,2.5,0.8509358);
+   gre->SetPointError(0,0,0.02384968);
+   gre->SetPoint(1,7.5,0.8590075);
+   gre->SetPointError(1,0,0.01109992);
+   gre->SetPoint(2,12.5,0.856496);
+   gre->SetPointError(2,0,0.007338999);
+   gre->SetPoint(3,17.5,0.8540863);
+   gre->SetPointError(3,0,0.007740469);
+   gre->SetPoint(4,22.5,0.8455393);
+   gre->SetPointError(4,0,0.0126823);
+   gre->SetPoint(5,27.5,0.8245856);
+   gre->SetPointError(5,0,0.03374804);
+   
+   TH1F *Graph_Graph3 = new TH1F("Graph_Graph3","Graph",100,0,30);
+   Graph_Graph3->SetMinimum(0.7824428);
+   Graph_Graph3->SetMaximum(0.8831803);
+   Graph_Graph3->SetDirectory(0);
+   Graph_Graph3->SetStats(0);
+   Graph_Graph3->GetXaxis()->SetNdivisions(505);
+   Graph_Graph3->GetXaxis()->SetLabelFont(42);
+   Graph_Graph3->GetXaxis()->SetLabelSize(0.05);
+   Graph_Graph3->GetXaxis()->SetTitleSize(0.05);
+   Graph_Graph3->GetXaxis()->SetTitleOffset(1.1);
+   Graph_Graph3->GetXaxis()->SetTitleFont(42);
+   Graph_Graph3->GetYaxis()->SetNdivisions(505);
+   Graph_Graph3->GetYaxis()->SetLabelFont(42);
+   Graph_Graph3->GetYaxis()->SetLabelSize(0.05);
+   Graph_Graph3->GetYaxis()->SetTitleSize(0.05);
+   Graph_Graph3->GetYaxis()->SetTitleOffset(1.5);
+   Graph_Graph3->GetYaxis()->SetTitleFont(42);
+   Graph_Graph3->GetZaxis()->SetNdivisions(707);
+   Graph_Graph3->GetZaxis()->SetLabelFont(42);
+   Graph_Graph3->GetZaxis()->SetLabelOffset(0.015);
+   Graph_Graph3->GetZaxis()->SetLabelSize(0.05);
+   Graph_Graph3->GetZaxis()->SetTitleSize(0.05);
+   Graph_Graph3->GetZaxis()->SetTitleOffset(1.1);
+   Graph_Graph3->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph3);
+   
+   gre->Draw("lp");
+   
+   gre = new TGraphErrors(6);
+   gre->SetName("Graph");
+   gre->SetTitle("Graph");
+   gre->SetFillColor(1);
+
+   ci = TColor::GetColor("#3300cc");
+   gre->SetLineColor(ci);
+
+   ci = TColor::GetColor("#3300cc");
+   gre->SetMarkerColor(ci);
+   gre->SetMarkerStyle(25);
+   gre->SetPoint(0,2.5,0.940508);
+   gre->SetPointError(0,0,0.02507353);
+   gre->SetPoint(1,7.5,0.9465003);
+   gre->SetPointError(1,0,0.0116515);
+   gre->SetPoint(2,12.5,0.9493146);
+   gre->SetPointError(2,0,0.007726436);
+   gre->SetPoint(3,17.5,0.9504735);
+   gre->SetPointError(3,0,0.008165568);
+   gre->SetPoint(4,22.5,0.942743);
+   gre->SetPointError(4,0,0.01339145);
+   gre->SetPoint(5,27.5,0.9378453);
+   gre->SetPointError(5,0,0.0359912);
+   
+   TH1F *Graph_Graph4 = new TH1F("Graph_Graph4","Graph",100,0,30);
+   Graph_Graph4->SetMinimum(0.8946559);
+   Graph_Graph4->SetMaximum(0.9810347);
+   Graph_Graph4->SetDirectory(0);
+   Graph_Graph4->SetStats(0);
+   Graph_Graph4->GetXaxis()->SetNdivisions(505);
+   Graph_Graph4->GetXaxis()->SetLabelFont(42);
+   Graph_Graph4->GetXaxis()->SetLabelSize(0.05);
+   Graph_Graph4->GetXaxis()->SetTitleSize(0.05);
+   Graph_Graph4->GetXaxis()->SetTitleOffset(1.1);
+   Graph_Graph4->GetXaxis()->SetTitleFont(42);
+   Graph_Graph4->GetYaxis()->SetNdivisions(505);
+   Graph_Graph4->GetYaxis()->SetLabelFont(42);
+   Graph_Graph4->GetYaxis()->SetLabelSize(0.05);
+   Graph_Graph4->GetYaxis()->SetTitleSize(0.05);
+   Graph_Graph4->GetYaxis()->SetTitleOffset(1.5);
+   Graph_Graph4->GetYaxis()->SetTitleFont(42);
+   Graph_Graph4->GetZaxis()->SetNdivisions(707);
+   Graph_Graph4->GetZaxis()->SetLabelFont(42);
+   Graph_Graph4->GetZaxis()->SetLabelOffset(0.015);
+   Graph_Graph4->GetZaxis()->SetLabelSize(0.05);
+   Graph_Graph4->GetZaxis()->SetTitleSize(0.05);
+   Graph_Graph4->GetZaxis()->SetTitleOffset(1.1);
+   Graph_Graph4->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph4);
+   
+   gre->Draw("lp");
+   
+   TLegend *leg = new TLegend(0.55,0.77,0.8,0.85,NULL,"brNDC");
+   leg->SetBorderSize(0);
+   leg->SetTextSize(0.03);
+   leg->SetLineColor(1);
+   leg->SetLineStyle(1);
+   leg->SetLineWidth(1);
+   leg->SetFillColor(19);
+   leg->SetFillStyle(0);
+   TLegendEntry *entry=leg->AddEntry("Graph","p_{T} > 20 GeV","lp");
+
+   ci = TColor::GetColor("#660066");
+   entry->SetLineColor(ci);
+   entry->SetLineStyle(1);
+   entry->SetLineWidth(1);
+
+   ci = TColor::GetColor("#660066");
+   entry->SetMarkerColor(ci);
+   entry->SetMarkerStyle(20);
+   entry->SetMarkerSize(1);
+   entry=leg->AddEntry("Graph","p_{T} > 30 GeV","lp");
+
+   ci = TColor::GetColor("#3300cc");
+   entry->SetLineColor(ci);
+   entry->SetLineStyle(1);
+   entry->SetLineWidth(1);
+
+   ci = TColor::GetColor("#3300cc");
+   entry->SetMarkerColor(ci);
+   entry->SetMarkerStyle(21);
+   entry->SetMarkerSize(1);
+   leg->Draw();
+   TLatex *   tex = new TLatex(0.2,0.87,"ATLAS");
+tex->SetNDC();
+   tex->SetTextFont(72);
+   tex->SetTextSize(0.04);
+   tex->SetLineWidth(2);
+   tex->Draw();
+      tex = new TLatex(0.33,0.87,"Simulation Preliminary");
+tex->SetNDC();
+   tex->SetTextFont(42);
+   tex->SetTextSize(0.04);
+   tex->SetLineWidth(2);
+   tex->Draw();
+      tex = new TLatex(0.2,0.82,"qq' #rightarrow Hqq', H#rightarrowZZ#rightarrow 4l");
+tex->SetNDC();
+   tex->SetTextFont(42);
+   tex->SetTextSize(0.03);
+   tex->SetLineWidth(2);
+   tex->Draw();
+      tex = new TLatex(0.2,0.78,"Anti-k_{t} LCW+JES R=0.4");
+tex->SetNDC();
+   tex->SetTextFont(42);
+   tex->SetTextSize(0.03);
+   tex->SetLineWidth(2);
+   tex->Draw();
+      tex = new TLatex(0.2,0.74,"solid markers: inclusive");
+tex->SetNDC();
+   tex->SetTextFont(42);
+   tex->SetTextSize(0.03);
+   tex->SetLineWidth(2);
+   tex->Draw();
+      tex = new TLatex(0.2,0.7,"open markers: JVT>0.7");
+tex->SetNDC();
+   tex->SetTextFont(42);
+   tex->SetTextSize(0.03);
+   tex->SetLineWidth(2);
+   tex->Draw();
+   c1->Modified();
+   c1->cd();
+   c1->SetSelected(c1);
+}
